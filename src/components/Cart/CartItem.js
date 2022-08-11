469c965fc729f5c2ae5a53efe3c45b752bbdf71b
MusUniver <lebusiness@mail.ru>
2022-08-11 16:02:44 +0300

    fetch api meals-data

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -7,7 +7,7 @@ const CartItem = ({name, price, amount, onRemove, onAdd}) => {
       <div>
         <h2>{name}</h2>
         <div className={classes.summary}>
-          <span className={classes.price}>{price}</span>
+          <span className={classes.price}>{price}₽</span>
           <span className={classes.amount}>x {amount}</span>
         </div>
       </div>
--- src/components/Meals/MealsList.js
+++ src/components/Meals/MealsList.js
@@ -1,36 +1,44 @@
+import { useEffect, useState } from "react";
 import Card from "./../UI/Card";
 import MealItem from "./MealItem/MealItem";
 import classes from "./MealsList.module.css";
 
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Суши",
-    description: "Лучшая рыба и овощи",
-    price: 450,
-  },
-  {
-    id: "m2",
-    name: "Шницель",
-    description: "Немецкое фирменное блюдо!",
-    price: 490,
-  },
-  {
-    id: "m3",
-    name: "Бургер с барбекю",
-    description: "Вкуснейший и сочный бургер",
-    price: 360,
-  },
-  {
-    id: "m4",
-    name: "Рамен",
-    description: "Японское блюдо с пшеничной лапшой",
-    price: 340,
-  },
-];
-
 const MealsList = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([]);
+  const [httpEror, setHttpEror] = useState(null);
+
+  useEffect(() => {
+    const featchMeals = async () => {
+      const response = await fetch(
+        "https://ordermeal-c0474-default-rtdb.europe-west1.firebasedatabase.app/meals.json"
+      );
+
+      if(!response.ok) {
+        throw new Error('Что-то пошло не так...');
+      }
+
+      const responseData = await response.json();
+
+      const loadedMeals = [];
+
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+      setMeals(loadedMeals);
+    };
+
+    featchMeals().catch(error => {
+      setHttpEror(error.message);
+    })
+    
+  }, []);
+
+  const mealsList = meals.map((meal) => (
     <MealItem
       id={meal.id}
       key={meal.id}
@@ -40,12 +48,27 @@ const MealsList = () => {
     />
   ));
 
+  if (httpEror) {
+    return <p className={classes.error}>{httpEror}</p>
+  }
   return (
-    <section className={classes.meals}>
-      <Card>
-        <ul>{mealsList}</ul>
-      </Card>
-    </section>
+    <>
+      {meals.length === 0 && (
+        <div class={classes['lds-ring']}>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      )}
+      {meals.length !== 0 && (
+        <section className={classes.meals}>
+          <Card>
+            <ul>{mealsList}</ul>
+          </Card>
+        </section>
+      )}
+    </>
   );
 };
 
